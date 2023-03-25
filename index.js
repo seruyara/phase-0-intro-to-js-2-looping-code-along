4f5893c0bbf477dd274c8e0250e0cdf8c515cd0f
Sheila Seruya <Sheilaseruya@gmail.com>
2023-03-25 19:18:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouCards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards;
+  } 
+const cards = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(cards);
+
+function countDown(num){
+    for (let i = num; i >= 0;i--){
+        console.log(i);
+    }
+}
